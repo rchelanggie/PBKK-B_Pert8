b090aacaa223601c49710a591121398ed28b1a7a
rchelanggie <rachelpasorong@gmail.com>
2023-12-28 22:12:26 +0700

    Initial Commit

---
+++ application/views/about.php
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<p>Nama: Rachel Anggieuli AP <br>
+Kelas: PBKK B
+</p>
---
+++ application/views/contact.php
@@ -0,0 +1,4 @@
+<h1>Contact</h1>
+<p>github: https://github.com/rchelanggie <br>
+blogspot: https://rachelpasorong.blogspot.com/
+</p>
